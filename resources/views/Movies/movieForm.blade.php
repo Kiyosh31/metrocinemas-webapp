3c77010b340ab74bc5ecc4edcb525ab158269370
David Garcia <david_5043@hotmail.com>
2019-03-14 10:45:14 -0600

    formatted code

--- resources/views/Movies/movieForm.blade.php
+++ resources/views/Movies/movieForm.blade.php
@@ -1,71 +1,71 @@
-@extends('layouts.tabler')
+@extends('layouts.tabler') 
 @section('content')
-    <div class="plage-header">
-        <h1>Agregar pelicula</h1>
-    </div>
-    <div class="row">
-        <div class="col-md-8 offset-2">
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title">Agregar pelicula</h3>
-                </div>
-                <div class="card-body">
-                    <form action="{{ route('movies.store') }}" method="POST">
-                        @csrf
-                        <div class="col-8 offset-2">
-                            <div class="form-group">
-                                <label class="form-label">Titulo</label>
-                                <input type="text" class="form-control" name="title" placeholder="Titulo">
-                            </div>
+<div class="plage-header">
+    <h1>Agregar pelicula</h1>
+</div>
+<div class="row">
+    <div class="col-md-8 offset-2">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Agregar pelicula</h3>
+            </div>
+            <div class="card-body">
+                <form action="{{ route('movies.store') }}" method="POST">
+                    @csrf
+                    <div class="col-8 offset-2">
+                        <div class="form-group">
+                            <label class="form-label">Titulo</label>
+                            <input type="text" class="form-control" name="title" placeholder="Titulo">
+                        </div>
 
-                            <div class="form-group">
-                                    <label class="form-label">Descripcion</label>
-                                    <input type="text" class="form-control" name="description" placeholder="Descripcion">
-                            </div>
+                        <div class="form-group">
+                            <label class="form-label">Descripcion</label>
+                            <input type="text" class="form-control" name="description" placeholder="Descripcion">
+                        </div>
 
-                            <div class="form-group">
-                                    <label class="form-label">Director</label>
-                                    <input type="text" class="form-control" name="director" placeholder="Director">
-                            </div>
+                        <div class="form-group">
+                            <label class="form-label">Director</label>
+                            <input type="text" class="form-control" name="director" placeholder="Director">
+                        </div>
 
-                            <div class="form-group">
-                                    <label class="form-label">Reparto</label>
-                                    <input type="text" class="form-control" name="cast" placeholder="Reparto">
-                            </div>
+                        <div class="form-group">
+                            <label class="form-label">Reparto</label>
+                            <input type="text" class="form-control" name="cast" placeholder="Reparto">
+                        </div>
 
-                            <div class="form-group">
-                                    <label class="form-label">Descripcion</label>
-                                    <input type="text" class="form-control" name="description" placeholder="Descripcion">
-                            </div>
+                        <div class="form-group">
+                            <label class="form-label">Descripcion</label>
+                            <input type="text" class="form-control" name="description" placeholder="Descripcion">
+                        </div>
 
-                            <div class="form-group">
-                                    <label class="form-label">Clasificacion</label>
-                                    <input type="text" class="form-control" name="clasification" placeholder="Clasificacion">
-                            </div>
+                        <div class="form-group">
+                            <label class="form-label">Clasificacion</label>
+                            <input type="text" class="form-control" name="clasification" placeholder="Clasificacion">
+                        </div>
 
-                            <div class="form-group">
-                                    <label class="form-label">Duracion</label>
-                                    <input type="text" class="form-control" name="duration" placeholder="Duracion">
-                            </div>
+                        <div class="form-group">
+                            <label class="form-label">Duracion</label>
+                            <input type="text" class="form-control" name="duration" placeholder="Duracion">
+                        </div>
 
-                            <div class="form-label">Estatus</div>
-                                <div class="custom-controls-stacked">
-                                <label class="custom-control custom-radio">
+                        <div class="form-label">Estatus</div>
+                        <div class="custom-controls-stacked">
+                            <label class="custom-control custom-radio">
                                     <input type="radio" class="custom-control-input" name="status" value="1" checked>
                                     <div class="custom-control-label">Activo</div>
                                 </label>
-                                <label class="custom-control custom-radio">
+                            <label class="custom-control custom-radio">
                                     <input type="radio" class="custom-control-input" name="status" value="0">
                                     <div class="custom-control-label">Inactivo</div>
                                 </label>
-                            </div>
-
-                                <button type="submit" class="btn btn-primary ml-auto">Aceptar</button>
-                            </div>
                         </div>
-                    </form>
-                </div>
+
+                        <button type="submit" class="btn btn-primary ml-auto">Aceptar</button>
+                    </div>
             </div>
+            </form>
         </div>
     </div>
+</div>
+</div>
 @endsection
--- resources/views/Movies/movieIndex.blade.php
+++ resources/views/Movies/movieIndex.blade.php
@@ -1,51 +1,50 @@
-@extends('layouts.tabler')
+@extends('layouts.tabler') 
 @section('content')
-    <div class="plage-header">
-        <h1>Listado de peliculas</h1>
-    </div>
-    <div class="row">
-        <div class="col-md-8 offset-2">
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title">Listado de peliculas</h3>
-                </div>
-                <div class="card-body">
-                    <div class="row">
-                            <table class="table table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">ID</th>
-                                        <th scope="col">Titulo</th>
-                                        <th scope="col">Descripcion</th>
-                                        <th scope="col">Director</th>
-                                        <th scope="col">Cast</th>
-                                        <th scope="col">Clasificacion</th>
-                                        <th scope="col">Duracion</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @if($movies->isEmpty())
-                                        <div class="alert alert-danger" role="alert">
-                                            No se encontraron peliculas
-                                        </div>
-                                    @else @foreach($movies as $movie)
-                                        <tr>
-                                            <td>{{ $movie->id }}</td>
-                                            <td>{{ $movie->title }}</td>
-                                            <td>{{ $movie->description }}</td>
-                                            <td>{{ $movie->director }}</td>
-                                            <td>{{ $movie->cast }}</td>
-                                            <td>{{ $movie->clasification }}</td>
-                                            <td>{{ $movie->duration_min }} minutos</td>
-                                        </tr>
-                                @endforeach 
-                                @endif
-                    
-                            </tbody>
-                        </table>
-                    </div>
+<div class="plage-header">
+    <h1>Listado de peliculas</h1>
+</div>
+<div class="row">
+    <div class="col-md-8 offset-2">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Listado de peliculas</h3>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Titulo</th>
+                                <th scope="col">Descripcion</th>
+                                <th scope="col">Director</th>
+                                <th scope="col">Cast</th>
+                                <th scope="col">Clasificacion</th>
+                                <th scope="col">Duracion</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @if($movies->isEmpty())
+                            <div class="alert alert-danger" role="alert">
+                                No se encontraron peliculas
+                            </div>
+                            @else @foreach($movies as $movie)
+                            <tr>
+                                <td>{{ $movie->id }}</td>
+                                <td>{{ $movie->title }}</td>
+                                <td>{{ $movie->description }}</td>
+                                <td>{{ $movie->director }}</td>
+                                <td>{{ $movie->cast }}</td>
+                                <td>{{ $movie->clasification }}</td>
+                                <td>{{ $movie->duration_min }} minutos</td>
+                            </tr>
+                            @endforeach @endif
+
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
